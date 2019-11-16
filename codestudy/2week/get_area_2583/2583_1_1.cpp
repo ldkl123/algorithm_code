2b9c264311c8bda7a22f27e01686ecb7c388ee53
Lee Do Kyu <dfdf55dfdf@naver.com>
2019-11-16 21:26:51 +0900

    changed typedef to pair

--- codestudy/2week/get_area_2583/2583_1_1.cpp
+++ codestudy/2week/get_area_2583/2583_1_1.cpp
@@ -8,16 +8,11 @@
 #include <algorithm>
 using namespace std;
 
-typedef struct dot{
-    int x;
-    int y;
-};
-
 bool map[101][101]={0,};
 bool visit[101][101] = {0,};
 void print_out1(int n, int m);
 void print_out2(int n, int m);
-void make_map(dot d1, dot d2);
+void make_map(pair<int, int> d1,  pair<int, int> d2);
 int BFS(int i, int j, int N, int M);
 int x_move[4] = {-1, 0, 0, 1};
 int y_move[4] = {0, -1, 1, 0};
@@ -27,15 +22,16 @@ int main(){
     scanf("%d %d %d", &M, &N, &K);
     cin.ignore();
     for(int i=0; i<K; i++){
-        string s;
-        dot d1; dot d2;
-        getline(cin, s);
-        d1.x = s[0]+1-'0';
-        d1.y = s[2]+1-'0';
-        d2.x = s[4]-'0';
-        d2.y = s[6]-'0';
-        map[d1.y][d1.x]= 1;
-        map[d2.y][d2.x] = 1;    
+        vector<int> v;
+        for(int j=0; j<4; j++){
+            int temp=0;
+            cin>>temp;
+            v.push_back(temp);
+        } 
+        pair<int, int> d1 = make_pair(v[0]+1, v[1]+1); 
+        pair<int, int> d2 = make_pair(v[2], v[3]); 
+        map[d1.second][d1.first]= 1;
+        map[d2.second][d2.first] = 1;    
         make_map(d1, d2);
     }
     for(int i=1; i<=N; i++){
@@ -47,6 +43,9 @@ int main(){
             }
         }
     }
+    //print_out1(M,N);
+    //cout << endl;
+    //print_out2(M,N);
     sort(res.begin(), res.end());
     cout << res.size() << endl;
     for(int i=0; i<res.size(); i++){
@@ -71,8 +70,8 @@ void print_out2(int n, int m){
         cout << endl;
     }
 }
-void make_map(dot d1, dot d2){
-    queue<dot> q;
+void make_map(pair<int, int> d1, pair<int, int> d2){
+    queue<pair<int, int> t> q;
     q.push(d1);
     q.push(d2);
     while(!q.empty()){
@@ -82,30 +81,26 @@ void make_map(dot d1, dot d2){
         for(int i=0; i<4; i++){
             int _x = x+x_move[i]; 
             int _y = y+y_move[i]; 
-            if(_x < d1.x || _x > d2.x || _y < d1.y || _y > d2.y){
+            if(_x < d1.first || _x > d2.first || _y < d1.second || _y > d2.second){
                 continue;
             }
             if(map[_y][_x] == 0){
                 map[_y][_x] = 1;
-                dot d;
-                d.x = _x;
-                d.y = _y;
+                pair<int, int> d = make_pair(_x, _y); 
                 q.push(d); 
             }
         }   
     } 
 }
 int BFS(int i, int j, int N, int M){
-    queue<dot>q;
+    queue<pair<int, int> >q;
     int ans=1;
-    dot d;
-    d.x = i;
-    d.y = j;
+    pair<int, int> d = make_pair(i, j);
     q.push(d);
     visit[j][i] = true;
     while(!q.empty()){
-        int x = q.front().x;
-        int y = q.front().y;
+        int x = q.front().first;
+        int y = q.front().second;
         q.pop();
         for(int i=0; i<4; i++){
             int _x = x+x_move[i];
@@ -115,9 +110,7 @@ int BFS(int i, int j, int N, int M){
             }
             if(map[_y][_x] == 0 && visit[_y][_x] == false){
                 visit[_y][_x] = true;
-                dot d;
-                d.x = _x;
-                d.y = _y;
+                pair<int, int> d = make_pair(_x, _y); 
                 q.push(d);
                 ans++;
             }
