fae8038fb55d808c631bdd4c165b3af5329f6aa4
Lee Do Kyu <dfdf55dfdf@naver.com>
2019-11-21 14:50:36 +0900

    2019.11.21

---
+++ codestudy/3week/superior_13458/13458_1_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <cstdio>
+#include <math.h>
+#include <cstdlib>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int N=0;
+    int B=0, C=0;
+    __int64_t ans=0;   // answer can be bigger than int's interval
+    scanf("%d", &N);
+    int* people = new int[N];
+    for(int i=0; i<N; i++){
+        scanf("%d", people+i);
+    }
+    scanf("%d %d", &B, &C);
+    for(int i=0; i<N; i++){
+        int num=1;
+        int temp = people[i] - B;  
+        if(temp> 0){
+            num+= temp/C;
+            if(temp%C > 0){
+                num++;
+            }
+        }
+        ans+=num;
+    }
+    cout << ans << "\n";
+}
